ffd2c4b9f74116b4cf1ec88a6ff3f3b55f7f6d72
Guilherme Lepsch <lepsch@gmail.com>
2022-03-15 19:15:54 +0000

    Rio das Pedras gallery

--- app/galleries/index.ts
+++ app/galleries/index.ts
@@ -2,10 +2,12 @@ import homeMadeWitches from './home-made-witches'
 import burano from './burano'
 import pipa from './pipa'
 import portugalPequeno from './portugal-pequeno'
+import rioDasPedras from './rio-das-pedras'
 
 export default [
   homeMadeWitches,
   burano,
   pipa,
   portugalPequeno,
+  rioDasPedras,
 ]
---
+++ app/galleries/rio-das-pedras.ts
@@ -0,0 +1,104 @@
+import Gallery from './Gallery'
+
+import _0 from '~/images/rio-das-pedras/0.jpg'
+import _01 from '~/images/rio-das-pedras/01.jpg'
+import _02 from '~/images/rio-das-pedras/02.jpg'
+import _03 from '~/images/rio-das-pedras/03.jpg'
+import _04 from '~/images/rio-das-pedras/04.jpg'
+import _05 from '~/images/rio-das-pedras/05.jpg'
+import _06 from '~/images/rio-das-pedras/06.jpg'
+import _07 from '~/images/rio-das-pedras/07.jpg'
+import _08 from '~/images/rio-das-pedras/08.jpg'
+import _09 from '~/images/rio-das-pedras/09.jpg'
+import _10 from '~/images/rio-das-pedras/10.jpg'
+import _11 from '~/images/rio-das-pedras/11.jpg'
+import _12 from '~/images/rio-das-pedras/12.jpg'
+import _13 from '~/images/rio-das-pedras/13.jpg'
+import _14 from '~/images/rio-das-pedras/14.jpg'
+
+import _0_t from '~/images/rio-das-pedras/thumbnails/0.jpg'
+import _01_t from '~/images/rio-das-pedras/thumbnails/01.jpg'
+import _02_t from '~/images/rio-das-pedras/thumbnails/02.jpg'
+import _03_t from '~/images/rio-das-pedras/thumbnails/03.jpg'
+import _04_t from '~/images/rio-das-pedras/thumbnails/04.jpg'
+import _05_t from '~/images/rio-das-pedras/thumbnails/05.jpg'
+import _06_t from '~/images/rio-das-pedras/thumbnails/06.jpg'
+import _07_t from '~/images/rio-das-pedras/thumbnails/07.jpg'
+import _08_t from '~/images/rio-das-pedras/thumbnails/08.jpg'
+import _09_t from '~/images/rio-das-pedras/thumbnails/09.jpg'
+import _10_t from '~/images/rio-das-pedras/thumbnails/10.jpg'
+import _11_t from '~/images/rio-das-pedras/thumbnails/11.jpg'
+import _12_t from '~/images/rio-das-pedras/thumbnails/12.jpg'
+import _13_t from '~/images/rio-das-pedras/thumbnails/13.jpg'
+import _14_t from '~/images/rio-das-pedras/thumbnails/14.jpg'
+
+const images: Gallery['images'] = [
+  {
+    original: _0,
+    thumbnail: _0_t,
+  },
+  {
+    original: _01,
+    thumbnail: _01_t,
+  },
+  {
+    original: _02,
+    thumbnail: _02_t,
+  },
+  {
+    original: _03,
+    thumbnail: _03_t,
+  },
+  {
+    original: _04,
+    thumbnail: _04_t,
+  },
+  {
+    original: _05,
+    thumbnail: _05_t,
+  },
+  {
+    original: _06,
+    thumbnail: _06_t,
+  },
+  {
+    original: _07,
+    thumbnail: _07_t,
+  },
+  {
+    original: _08,
+    thumbnail: _08_t,
+  },
+  {
+    original: _09,
+    thumbnail: _09_t,
+  },
+  {
+    original: _10,
+    thumbnail: _10_t,
+  },
+  {
+    original: _11,
+    thumbnail: _11_t,
+  },
+  {
+    original: _12,
+    thumbnail: _12_t,
+  },
+  {
+    original: _13,
+    thumbnail: _13_t,
+  },
+  {
+    original: _14,
+    thumbnail: _14_t,
+  },
+]
+
+const gallery: Gallery = {
+  id: 'rio-das-pedras',
+  images,
+  title: 'Rio das Pedras',
+}
+
+export default gallery
